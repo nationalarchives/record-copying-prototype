13ec0a8b8672bd5b4e6e3f9d662e38421262b7fb
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-07 11:56:17 +0000

    Addition of files
    Addition of main PHP, CSS and JS files for wireframe

--- details-page.php
+++ details-page.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en-gb">
-<?php require_once('includes/head.php'); ?>
+    <?php require_once('includes/head.php'); ?>
     <body>
         <?php require_once('includes/header.php'); ?>
         <main id="page_wrap" class="container" role="main">
-            <?php require_once('includes/index-page-content.php'); ?>
+            <?php require_once('includes/details-page-content.php'); ?>
         </main>
         <?php require_once('includes/footer.php'); ?>
         <?php require_once('includes/footer-scripts.php'); ?>
---
+++ includes/index-page-content.php
@@ -0,0 +1,16 @@
+<div class="col starts-at-full clr holding-box">
+    <div class="heading-holding-banner">
+        <h2><span><span>Record copying wireframe</span></span></h2>
+    </div>
+    <div class="breather">
+        <p>This is a clickable prototype representing a single path through the record copying process. While there are some dynamic elements, it has no 'back-end', data, and is not an application.</p>
+    </div>
+    <div class="breather">
+        <h3 class="inline">Prototype pages</h3>
+        <ul>
+            <li>
+                <a href="details-page.php">Start page - Information asset details</a>
+            </li>
+        </ul>
+    </div>
+</div>
