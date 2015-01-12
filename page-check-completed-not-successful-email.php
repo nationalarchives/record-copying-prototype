a6416a1076b8b8b4ca57fadc080bf297671912e3
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-12 13:21:15 +0000

    Addition of page-check completed emails (successful and unsuccessful)

---
+++ page-check-completed-not-successful-email.php
@@ -0,0 +1,15 @@
+<?php require_once('includes/basket-logic.php'); ?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<?php require_once('includes/head.php'); ?>
+<body ng-app="recordCopying">
+<main id="page_wrap" class="container" role="main">
+    <div class="email-mock">
+        <p>Dear x,</p>
+        <p>Your page check has been completed, but we're sorry to tell you that it hasn't been successful. This is because [explanation]</p>
+        <p>You can pay someone to carry out research on your behalf, or you can come to The National Archives to view the original records for free. Alternatively, you may wish to submit a new request.</p>
+        <p>The records copying team</p>
+    </div>
+</main>
+</body>
+</html>
---
+++ page-check-completed-successful-email.php
@@ -0,0 +1,16 @@
+<?php require_once('includes/basket-logic.php'); ?>
+<!DOCTYPE html>
+<html lang="en-gb">
+<?php require_once('includes/head.php'); ?>
+<body ng-app="recordCopying">
+<main id="page_wrap" class="container" role="main">
+    <div class="email-mock">
+        <p>Dear x,</p>
+        <p>Your page check has been completed, and we're pleased to tell you that it has been successful. If you would like to proceed with your order, please visit <a href="your-orders.php">Your orders</a> to view the delivery options and costs.</p>
+        <p>You have 4 weeks to proceed with your order before it expires.</p>
+        <p>If you do not want to proceed, you can cancel it by visiting <a href="your-orders.php">Your orders</a>.</p>
+        <p>The records copying team</p>
+    </div>
+</main>
+</body>
+</html>
