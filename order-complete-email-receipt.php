cd65f612d2010aee2392b9137cdcae5b2e37c789
Chris Bishop <domingobishop@gmail.com>
2015-08-21 14:55:20 +0100

    added receipt email

---
+++ order-complete-email-receipt.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<?php require_once('includes/head.php'); ?>
+<body ng-app="recordCopying">
+<main id="page_wrap" class="container" role="main">
+    <div class="email-mock">
+        <p>Dear x,</p>
+        <p>Your copy of (catalogue reference) has now been dispatched. Your unique reference number for this order is
+            x.</p>
+        <p>The records copying team</p>
+    </div>
+</main>
+</body>
+</html>
