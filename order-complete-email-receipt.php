80ee7eae6e417317c011fa5e5d0e901623e284b9
Chris Bishop <domingobishop@gmail.com>
2015-08-21 14:55:50 +0100

    added content

--- order-complete-email-receipt.php
+++ order-complete-email-receipt.php
@@ -4,10 +4,61 @@
 <body ng-app="recordCopying">
 <main id="page_wrap" class="container" role="main">
     <div class="email-mock">
-        <p>Dear x,</p>
-        <p>Your copy of (catalogue reference) has now been dispatched. Your unique reference number for this order is
-            x.</p>
-        <p>The records copying team</p>
+        <img src="http://www.nationalarchives.gov.uk/images/global/hdr-logo.gif" alt="The National Archives">
+        <p>Thank you for your order.</p>
+        <p> <strong>Order reference number:</strong> I/116938265204068X </p>
+        <p> <strong>Order date:</strong> 21/08/2015 10:58 am  </p>
+        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+            <h3>(2) Digital Item(s)</h3>
+        </div>
+        <div style="padding-top:10px; border-top:solid 1px #CCCCCC; ">
+            <p> <strong>Title: </strong>&nbsp;&nbsp;Felix Ruddick</p>
+            <p> <strong>Order item ID: </strong>&nbsp;&nbsp;784985415</p>
+            <p> <strong>Catalogue reference: </strong>&nbsp;&nbsp;WO 409/27/33/804</p>
+            <p> <strong>Price: </strong>&nbsp;&nbsp;&nbsp;£3.00</p>
+            <p> <a href="#">Click here to download your order item</a></p>
+        </div>
+        <div style="padding-top:10px; border-top:solid 1px #CCCCCC; ">
+            <p> <strong>Title: </strong>&nbsp;&nbsp;3rd Infantry Brigade Anti-tank Company</p>
+            <p> <strong>Order item ID: </strong>&nbsp;&nbsp;784985416</p>
+            <p> <strong>Catalogue reference: </strong>&nbsp;&nbsp;WO 33/804/34/1</p>
+            <p> <strong>Price: </strong>&nbsp;&nbsp;&nbsp;£3.00</p>
+            <p> <a href="#">Click here to download your order item</a></p>
+        </div>
+        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+            <h3>(1) Closed records research request(s)</h3>
+        </div>
+        <div style="padding-top:10px; border-top:solid 1px #CCCCCC; ">
+            <p> <strong>Title:</strong>&nbsp;&nbsp;James Alcock</p>
+            <p> <strong>Order item ID: </strong>&nbsp;&nbsp;784985417</p>
+            <p> <strong>Catalogue reference: </strong>&nbsp;&nbsp;WO 409/27/27/27</p>
+            <p> <strong>Price:</strong>&nbsp;&nbsp;&nbsp;£25.00</p>
+        </div>
+        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+            <h3>(1) Page check request(s)</h3>
+        </div>
+        <div style="padding-top:10px; border-top:solid 1px #CCCCCC; ">
+            <p> <strong>Title:</strong>&nbsp;&nbsp;Letters from Flag Officer West Africa Squadron</p>
+            <p> <strong>Order item ID: </strong>&nbsp;&nbsp;784985418</p>
+            <p> <strong>Catalogue reference: </strong>&nbsp;&nbsp;ADM 1/1</p>
+            <p> <strong>Price:</strong>&nbsp;&nbsp;&nbsp;£8.24</p>
+        </div>
+        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+            <h3><strong>Total :</strong>&nbsp;&nbsp; £39.24 </h3>
+        </div>
+        <p> You must download this record within 30 days from the order date.</p>
+        <p> If you were signed into your Discovery account when you made your order, your images are also available to download from your current orders.</p>
+        <p> If you have any questions or concerns about your order, please contact us at: digitaldownloads@nationalarchives.gsi.gov.uk  </p>
+        <p> In all communications about your order, please quote the order reference number at the top of this email. </p>
+        <p>
+            Sign up for our free enewsletter to receive regular updates from The National Archives:
+            <a href="http://www.nationalarchives.gov.uk/enewsletter"> www.nationalarchives.gov.uk/enewsletter</a>
+        </p>
+        <div style="padding:10px 0; border-top:solid 1px #CCCCCC; ">
+            <p><a href="http://www.nationalarchives.gov.uk">www.nationalarchives.gov.uk</a></p>
+            <p>The National Archives, Kew, Richmond, Surrey, TW9 4DU</p>
+            <p>Telephone: +44 (0)20 8876 3444 - Minicom: +44 (0)20 8392 9198</p>
+        </div>
     </div>
 </main>
 </body>
