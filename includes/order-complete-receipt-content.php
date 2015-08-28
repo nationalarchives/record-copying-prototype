7014516511fd38341a17a100c3da987e6c664a81
Chris Bishop <domingobishop@gmail.com>
2015-08-28 16:12:31 +0100

    new php files

---
+++ includes/order-complete-receipt-content.php
@@ -0,0 +1,104 @@
+<?php require_once('page-check-in-basket-breadcrumb.php'); ?>
+<section class="row" ng-controller="basketController">
+    <div class="col starts-at-full ends-at-two-thirds clr holding-box">
+
+
+            <div class="heading-holding-banner">
+                <h1>
+                    <span>
+                        <span> 
+                            Thank you for your order
+                        </span>
+                    </span>
+                </h1>
+            </div>
+            <div class="breather">
+                <ul class="step-indicator">
+                    <li>
+                        <span>1</span>
+                        <span>Basket</span>
+                    </li>
+                    <li>
+                        <span>2</span>
+                        <span>Review</span>
+                    </li>
+                    <li>
+                        <span>3</span>
+                        <span>Payment</span>
+                    </li>
+                    <li class="last active">
+                        <span>4</span>
+                        <span>Confirmation</span>
+                    </li>
+                </ul>
+
+                <div class="holding-box basket-contents">
+                    <h2 class="inline clr">Transaction reference: I/120860526404044M</h2>
+                    <div class="order-items">
+                        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+                            <h3>2 Digital Items</h3>
+                        </div>
+                        <div class="basket-item">
+                            <p> <strong>Title: </strong>&nbsp;&nbsp;Felix Ruddick<br>
+                                <strong>Order item ID: </strong>&nbsp;&nbsp;784985415<br>
+                                <strong>Catalogue reference: </strong>&nbsp;&nbsp;WO 409/27/33/804<br>
+                                <strong>Price: </strong>&nbsp;&nbsp;&nbsp;£3.00</p>
+                            <p> <a href="#">Click here to download your order item</a></p>
+                        </div>
+                        <div class="basket-item">
+                            <p> <strong>Title: </strong>&nbsp;&nbsp;3rd Infantry Brigade Anti-tank Company<br>
+                                <strong>Order item ID: </strong>&nbsp;&nbsp;784985416<br>
+                                <strong>Catalogue reference: </strong>&nbsp;&nbsp;WO 33/804/34/1<br>
+                                <strong>Price: </strong>&nbsp;&nbsp;&nbsp;£3.00</p>
+                            <p> <a href="#">Click here to download your order item</a></p>
+                        </div>
+                        <div class="emphasis-block">
+                            <p>You must download this record within 30 days from the order date.</p>
+                            <p>If you were signed into your Discovery account when you made your order, your images are also available to download from your current orders.</p>
+                            <p>If you have any questions or concerns about your order, please contact us at digitaldownloads@nationalarchives.gsi.gov.uk quoting the order ID number.</p>
+                        </div>
+                        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+                            <h3>Naturalisation Certificate</h3>
+                        </div>
+                        <div class="basket-item">
+                            <p> <strong>Title:</strong>&nbsp;&nbsp;Ho Shu-hing known as Ho Wai-ping known as Lily Ho...<br>
+                                <strong>Order item ID: </strong>&nbsp;&nbsp;784985420<br>
+                                <strong>Catalogue reference: </strong>&nbsp;&nbsp;HO 334/1983/48484<br>
+                                <strong>Price:</strong>&nbsp;&nbsp;&nbsp;£25.00</p>
+                        </div>
+                        <div class="emphasis-block">
+                            <p>You have chosen to receive a certified copy.</p>
+                            <p>Your order will be dispatched by Monday 14 September 2015.</p>
+                            <p>If you have any questions or concerns about your order, please contact us at +44 (0) 20 8876 3444.</p>
+                        </div>
+                        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+                            <h3>Closed records research request</h3>
+                        </div>
+                        <div class="basket-item">
+                            <p> <strong>Title:</strong>&nbsp;&nbsp;James Alcock<br>
+                                <strong>Order item ID: </strong>&nbsp;&nbsp;784985417<br>
+                                <strong>Catalogue reference: </strong>&nbsp;&nbsp;WO 409/27/27/27<br>
+                                <strong>Price:</strong>&nbsp;&nbsp;&nbsp;£25.00</p>
+                        </div>
+                        <div style="padding:10px 0; border-top:solid 3px #CCCCCC; ">
+                            <h3>Page check request</h3>
+                        </div>
+                        <div class="basket-item">
+                            <p> <strong>Title:</strong>&nbsp;&nbsp;Letters from Flag Officer West Africa Squadron<br>
+                                <strong>Order item ID: </strong>&nbsp;&nbsp;784985418<br>
+                                <strong>Catalogue reference: </strong>&nbsp;&nbsp;ADM 1/1<br>
+                                <strong>Price:</strong>&nbsp;&nbsp;&nbsp;£8.24</p>
+                        </div>
+                        <div class="emphasis-block">
+                            <p>You have chosen to receive a certified copy.</p>
+                            <p>If you have any questions or concerns about your page check, please contact us at +44 (0) 20 8876 3444.</p>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+    </div>
+</section>
